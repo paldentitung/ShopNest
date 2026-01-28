3ea86ea7a2bfef9aeaa0352ba344f87f87ab97ca
Palden D. Titung <paldendorjetitung@gmail.com>
2026-01-28 21:17:59 +0545

    add demo data in frontend

---
+++ frontend/src/data/productData.js
@@ -0,0 +1,127 @@
+import womenStretchPopoverHoodieBlack from "../assets/images-folder/products/women-stretch-popover-hoodie-black.jpg";
+import menGolfPoloTShirtBlue from "../assets/images-folder/products/men-golf-polo-t-shirt-blue.jpg";
+import womenChiffonBeachwearCoverupBlack from "../assets/images-folder/products/women-chiffon-beachwear-coverup-black.jpg";
+import adultsPlainCottonTshirt2PackTeal from "../assets/images-folder/products/adults-plain-cotton-tshirt-2-pack-teal.jpg";
+import menCozyFleeceZipUpHoodieBlack from "../assets/images-folder/products/variations/men-cozy-fleece-zip-up-hoodie-black.jpg";
+import athleticCottonSocks6Pairs from "../assets/images-folder/products/athletic-cotton-socks-6-pairs.jpg";
+const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    slug: "black-gray-athletic-cotton-socks-6-pairs",
+    category: "Clothing",
+    images: womenStretchPopoverHoodieBlack,
+
+    priceCents: 1499,
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    description:
+      "Experience all-day comfort with this 6-pack of black and gray athletic cotton socks, designed for both performance and casual wear.",
+    variations: {
+      colors: ["Black", "Gray"],
+      sizes: ["Free Size"],
+    },
+    stock: 120,
+  },
+
+  {
+    id: "a12b98de-23f1-4a0e-b921-12cda9e45f11",
+    name: "Men's Classic Cotton T-Shirt",
+    slug: "mens-classic-cotton-tshirt",
+    category: "Clothing",
+    images: menGolfPoloTShirtBlue,
+    priceCents: 1999,
+    rating: {
+      stars: 4,
+      count: 134,
+    },
+    description:
+      "A timeless cotton t-shirt crafted for everyday comfort and style. Soft, breathable, and perfect for casual wear.",
+    variations: {
+      colors: ["White", "Black", "Navy"],
+      sizes: ["S", "M", "L", "XL"],
+    },
+    stock: 80,
+  },
+
+  {
+    id: "f92a77d3-88cb-44a3-b78d-51c8b2198b29",
+    name: "adults Plain Cotton Tshirt2PackTeal",
+    slug: "adults Plain Cotton Tshirt2PackTeal",
+    category: "Clothing",
+    images: adultsPlainCottonTshirt2PackTeal,
+    priceCents: 3499,
+    rating: {
+      stars: 5,
+      count: 56,
+    },
+    description:
+      "Stylish high-waist denim jeans designed for a flattering fit and long-lasting comfort. Perfect for everyday outfits.",
+    variations: {
+      colors: ["Blue", "Black"],
+      sizes: ["26", "28", "30", "32"],
+    },
+    stock: 45,
+  },
+  {
+    id: "f92a77d3-88cb-44a3-b78d-51c8b2198b29",
+    name: "women Chiffon Beachwear Coverup Black",
+    slug: "womenChiffonBeachwearCoverupBlack",
+    category: "Clothing",
+    images: womenChiffonBeachwearCoverupBlack,
+    priceCents: 3499,
+    rating: {
+      stars: 5,
+      count: 56,
+    },
+    description:
+      "Stylish high-waist denim jeans designed for a flattering fit and long-lasting comfort. Perfect for everyday outfits.",
+    variations: {
+      colors: ["Blue", "Black"],
+      sizes: ["26", "28", "30", "32"],
+    },
+    stock: 45,
+  },
+  {
+    id: "f92a77d3-88cb-44a3-b78d-51c8b2198b29",
+    name: "menCozy Fleece Zip UpHoodie Black",
+    slug: "menCozyFleeceZipUpHoodieBlack",
+    category: "Clothing",
+    images: menCozyFleeceZipUpHoodieBlack,
+    priceCents: 3499,
+    rating: {
+      stars: 5,
+      count: 56,
+    },
+    description:
+      "Stylish high-waist denim jeans designed for a flattering fit and long-lasting comfort. Perfect for everyday outfits.",
+    variations: {
+      colors: ["Blue", "Black"],
+      sizes: ["26", "28", "30", "32"],
+    },
+    stock: 45,
+  },
+  {
+    id: "f92a77d3-88cb-44a3-b78d-51c8b2198b29",
+    name: "athletic Cotton Socks 6Pairs",
+    slug: "athleticCottonSocks6Pairs",
+    category: "Clothing",
+    images: athleticCottonSocks6Pairs,
+    priceCents: 3499,
+    rating: {
+      stars: 5,
+      count: 56,
+    },
+    description:
+      "Stylish high-waist denim jeans designed for a flattering fit and long-lasting comfort. Perfect for everyday outfits.",
+    variations: {
+      colors: ["Blue", "Black"],
+      sizes: ["26", "28", "30", "32"],
+    },
+    stock: 45,
+  },
+];
+
+export default products;
