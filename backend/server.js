44769bd8df294305315f81316aeb7ff0f205354d
Palden D. Titung <paldendorjetitung@gmail.com>
2026-01-28 21:55:11 +0545

    feat: connect mongodb with server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("hello from server");
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectdb = require("./config/db");
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+connectdb();
+
+app.listen(() => {
+  console.log(`server running in ${PORT}`);
+});
