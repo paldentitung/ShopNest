a624bdb7853a33b8e71a46637352b274817afd03
Palden D. Titung <paldendorjetitung@gmail.com>
2026-03-25 21:32:11 +0545

    feat: implement the get wishlist functionality

--- frontend/src/Components/Profile/Wishlist.jsx
+++ frontend/src/Components/Profile/Wishlist.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { getWishlist } from "../../Services/wishlistApi";
 const Wishlist = () => {
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    const fetchWishlist = async () => {
+      try {
+        const res = await getWishlist();
+
+        setWishlist(res.wishlist);
+      } catch (error) {
+        console.error("Fetch error:", error);
+        setWishlist([]);
+      }
+    };
+
+    fetchWishlist();
+  }, []);
   return (
     <div className="bg-white p-6 shadow-md rounded-md">
       <div className="flex justify-between items-center text-sm">
@@ -9,74 +28,29 @@ const Wishlist = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 p-3 gap-8">
-        <div className="flex flex-col gap-2 shadow overflow-hidden rounded-md group transition-all duration-300 hover:cursor-pointer relative">
-          <div className="overflow-hidden h-65">
-            <img
-              src="../hero-image-1.jpg"
-              alt=""
-              className="transition-all duration-300 group-hover:scale-110 w-full h-full object-cover"
-            />
-          </div>
-          <div className="flex flex-col text-sm px-2 py-4">
-            <span>Classic White Sneakers</span>
-            <span>$129.00</span>
-          </div>
+        {wishlist.map((w) => (
+          <div
+            key={w._id}
+            className="flex flex-col gap-2 shadow overflow-hidden rounded-md group transition-all duration-300 hover:cursor-pointer relative"
+          >
+            <div className="overflow-hidden h-65">
+              <img
+                src={`http://localhost:3000/${w.image[0]}`}
+                alt={`${w.name} `}
+                className="transition-all duration-300 group-hover:scale-110 w-full h-full object-contain"
+              />
+            </div>
 
-          <div className=" hidden group-hover:block  absolute bottom-1/4 right-2 p-2 bg-amber-600 rounded-full">
-            <FaShoppingCart color="white" size={20} />
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 shadow overflow-hidden rounded-md group transition-all duration-300 hover:cursor-pointer relative">
-          <div className="overflow-hidden h-65">
-            <img
-              src="../hero-image-1.jpg"
-              alt=""
-              className="transition-all duration-300 group-hover:scale-110 w-full h-full object-cover"
-            />
-          </div>
-          <div className="flex flex-col text-sm px-2 py-4">
-            <span>Classic White Sneakers</span>
-            <span>$129.00</span>
-          </div>
+            <div className="flex flex-col text-sm px-2 py-4">
+              <span>{w.name}</span>
+              <span>${(w.price / 100).toFixed(2)}</span>
+            </div>
 
-          <div className=" hidden group-hover:block  absolute bottom-1/4 right-2 p-2 bg-amber-600 rounded-full">
-            <FaShoppingCart color="white" size={20} />
+            <div className="hidden group-hover:block absolute bottom-1/4 right-2 p-2 bg-amber-600 rounded-full">
+              <FaShoppingCart color="white" size={20} />
+            </div>
           </div>
-        </div>{" "}
-        <div className="flex flex-col gap-2 shadow overflow-hidden rounded-md group transition-all duration-300 hover:cursor-pointer relative">
-          <div className="overflow-hidden h-65">
-            <img
-              src="../hero-image-1.jpg"
-              alt=""
-              className="transition-all duration-300 group-hover:scale-110 w-full h-full object-cover"
-            />
-          </div>
-          <div className="flex flex-col text-sm px-2 py-4">
-            <span>Classic White Sneakers</span>
-            <span>$129.00</span>
-          </div>
-
-          <div className=" hidden group-hover:block  absolute bottom-1/4 right-2 p-2 bg-amber-600 rounded-full">
-            <FaShoppingCart color="white" size={20} />
-          </div>
-        </div>{" "}
-        <div className="flex flex-col gap-2 shadow overflow-hidden rounded-md group transition-all duration-300 hover:cursor-pointer relative">
-          <div className="overflow-hidden h-65">
-            <img
-              src="../hero-image-1.jpg"
-              alt=""
-              className="transition-all duration-300 group-hover:scale-110 w-full h-full object-cover"
-            />
-          </div>
-          <div className="flex flex-col text-sm px-2 py-4">
-            <span>Classic White Sneakers</span>
-            <span>$129.00</span>
-          </div>
-
-          <div className=" hidden group-hover:block  absolute bottom-1/4 right-2 p-2 bg-amber-600 rounded-full">
-            <FaShoppingCart color="white" size={20} />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ frontend/src/Services/wishlistApi.js
@@ -0,0 +1,13 @@
+import toast from "react-hot-toast";
+import { apiFetch } from "../utils/api";
+export const getWishlist = async () => {
+  try {
+    const res = await apiFetch("/wishlist", {
+      method: "GET",
+    });
+    console.log("wishlist", res);
+    return res;
+  } catch (error) {
+    toast.error(error);
+  }
+};
