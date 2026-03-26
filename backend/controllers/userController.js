809cfd719c63e2064c4fab96380b4805c5a29d6d
Palden D. Titung <paldendorjetitung@gmail.com>
2026-03-26 13:05:55 +0545

    Feat: add the update profile functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,3 +8,39 @@ exports.getAllUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const avatar = req.file ? req.file.path : req.body.avatar;
+    const address = req.body.address;
+    const phone = req.body.phone;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { avatar, address, phone },
+      { new: true, runValidators: true },
+    ).select("-password");
+
+    if (!updatedUser)
+      return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({
+      message: "Profile updated",
+      user: {
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        phone: updatedUser.phone,
+        avatar: updatedUser.avatar,
+        address: updatedUser.address,
+        role: updatedUser.role,
+        status: updatedUser.status,
+        wishlist: updatedUser.wishlist,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
-const { getAllUser } = require("../controllers/userController");
+const { getAllUser, updateProfile } = require("../controllers/userController");
+const upload = require("../middleware/upload");
 const auth = require("../middleware/auth");
 const isAdmin = require("../middleware/isAdmin");
 
 const Router = require("express").Router();
 
 Router.get("/", auth, isAdmin, getAllUser);
+Router.post("/", upload.single("image"), auth, updateProfile);
 
 module.exports = Router;
