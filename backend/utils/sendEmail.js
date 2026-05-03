3897ce9cc247abeb91951612d69bfa3ec72ecf35
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-03 20:45:31 +0545

    fix: the email issue not sent  in live

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,7 +3,9 @@ const nodemailer = require("nodemailer");
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
